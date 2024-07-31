bb47dd0aeffabcb63693482e72da72edd5c1b384
Bishal Moktan <bishalmoktan270@gmail.com>
2024-07-31 19:35:05 +0545

    feat/navbar UI complete

---
+++ src/components/navbar/mobile-nav.tsx
@@ -0,0 +1,46 @@
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "../ui/sheet";
+import { Button } from "../ui/button";
+import logo from "../../../public/logo.svg";
+import Link from "next/link";
+import Image from "next/image";
+
+const MobileNav = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side={"left"} className="w-[85vw]">
+        <SheetHeader>
+            <SheetTitle>
+            <Link href={"/"} className="">
+                <Image src={logo} alt="logo" className="w-[200px] h-full object-cover" />
+            </Link>
+            </SheetTitle>
+        </SheetHeader>
+        <div className="py-4 flex flex-col gap-6 text-secondary-foreground font-extralight transition-all">
+          <Link href={"/"} className="hover:font-normal">
+            Explore Courses
+          </Link>
+          <Link href={"/"} className="hover:font-normal">
+            Success Stories
+          </Link>
+          <Link href={"/"} className="hover:font-normal">
+            Blogs
+          </Link>
+          <Link href={"/"} className="hover:font-normal">
+            Events
+          </Link>
+          <div className="flex justify-between">
+          <Button variant={'secondary'} size={'lg'}>Log in</Button>
+          <Button size={'lg'}>Start Free Trial</Button>
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
+export default MobileNav;
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image"
+import logo from "../../../public/logo.svg";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import MobileNav from "./mobile-nav";
+
+const Navbar = () => {
+  return (
+    <nav className="shadow-md">
+        <div className="container py-4 flex justify-between items-center">
+            <div className="md:hidden">
+            <MobileNav />
+            </div>
+            <Link href={"/"} className="w-[220px]">
+                <Image src={logo} alt="logo" className="w-full h-full object-cover" />
+            </Link>
+            <div className="hidden md:block space-x-6 text-secondary-foreground font-extralight transition-all">
+                <Link href={"/"} className="hover:font-normal">Explore Courses</Link>
+                <Link href={"/"} className="hover:font-normal">Success Stories</Link>
+                <Link href={"/"} className="hover:font-normal">Blogs</Link>
+                <Link href={"/"} className="hover:font-normal">Events</Link>
+            </div>
+            <div className="hidden md:block space-x-4">
+                <Button variant={'secondary'} size={'lg'}>Log in</Button>
+                <Button size={'lg'}>Start Free Trial</Button>
+            </div>
+        </div>
+    </nav>
+  )
+}
+export default Navbar
